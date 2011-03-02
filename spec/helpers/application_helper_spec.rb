b3d0d800f238a49f009e10bcd1db24cffb6fe44b
Britto <jabcalves@gmail.com>
2011-03-01 23:02:33 -0300

    Fixes the application helper implementation and tests.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,41 +4,16 @@ describe ApplicationHelper do
   describe "#tab_link_to" do
     subject { helper.tab_link_to('My Link', {:controller => 'sobre'}) }
 
-    context "matching params" do
-      before(:each) do
-        params[:controller] = 'sobre'
-        params[:junk] = 'junk'
-      end
-      it("mark tab as selected") { should have_tag('a[class=menu_select]') }
+    context "when the link points to the current page" do
+      before { helper.stub(:current_page?) { true } }
+
+      it("mark tab as selected") { should =~ /class="menu_select"/ }
     end
 
-    context "non-matching params" do
-      before(:each) do
-        params[:controller] = 'eventos'
-        params[:junk] = 'junk'
-      end
-      it("unmark tab") { should have_tag('a[class=menu_inicial]') }
-    end
+    context "when the link doesn't points to the current page" do
+      before { helper.stub(:current_page?) { false } }
 
-    context "url with both controller and action matching" do
-      subject { helper.tab_link_to('My Link', {:controller => 'sobre', :action => 'colaboradores'}) }
-      before(:each) do
-        params[:controller] = 'sobre'
-        params[:action] = 'colaboradores'
-        params[:junk] = 'junk'
-      end
-      it("don't mark tab as selected") { should have_tag('a[class=menu_select]') }
-    end
-
-    context "url with both controller and action, but action doesn't match" do
-      subject { helper.tab_link_to('My Link', {:controller => 'sobre', :action => 'colaboradores'}) }
-      before(:each) do
-        params[:controller] = 'sobre'
-        params[:action] = 'index'
-        params[:junk] = 'junk'
-      end
-      it("don't mark tab as selected") { should have_tag('a[class=menu_inicial]') }
-
+      it("unmark tab") { should =~ /class="menu_inicial"/ }
     end
   end
 end
