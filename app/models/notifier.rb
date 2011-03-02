292e8a0505be9bf8c5a597d1cd63a565a5e3777d
Lucas Mazza <luc4smazza@gmail.com>
2011-03-01 21:18:37 -0300

    Some improvements to the mailing stuff

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,14 +1,10 @@
 class Notifier < ActionMailer::Base
 
-  default :from => "contato@agendatech.com.br"
+  default :from => "contato@agendatech.com.br", :to => ["andersonlfl@gmail.com", "alots.ssa@gmail.com"]
 
-  def envia_email (contato)
-
+  def envia_email(contato)
     @contato = contato
-    mail( :subject => "Contato agendatech",
-          :to => ["andersonlfl@gmail.com", "alots.ssa@gmail.com"].join(",")
-    )
-    
+    mail(:subject => "Contato agendatech")
   end
 
 end
--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,9 +1,24 @@
 require "spec_helper"
 
 describe Notifier do
-  it "deveria enviar um email" do
-    contato = Contato.new :nome => "Alberto",:mensagem => "oi",:mensagem => "msg"
-    email = Notifier.envia_email(contato).deliver
-    ActionMailer::Base.deliveries.empty?.should be_false
+  let(:contato) {  Contato.new :nome => "Alberto",:mensagem => "oi",:mensagem => "msg" }
+
+  context "Enviando e-mail" do
+
+    subject { Notifier.envia_email(contato) }
+    let(:mensagem) { subject.deliver }
+
+    it "dispara o e-mail" do
+      expect { mensagem }.to change(ActionMailer::Base.deliveries, :size).by(1)
+    end
+
+    it "usa o assunto padrão" do
+      mensagem.subject.should == "Contato agendatech"
+    end
+
+    it "envia o e-mail para 2 recipientes" do
+      mensagem.to.should have(2).items
+    end
+
   end
 end
