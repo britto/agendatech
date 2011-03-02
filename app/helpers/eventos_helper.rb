0e1a0ed0330a401de41ee2a3e486e0f29929918c
Lucas Mazza <luc4smazza@gmail.com>
2011-03-01 22:05:17 -0300

    cleaning some nasty env checking

--- app/helpers/eventos_helper.rb
+++ app/helpers/eventos_helper.rb
@@ -6,19 +6,19 @@ module EventosHelper
   def evento_full_path(evento)
     evento_path(:ano => evento.data.year,:id=>evento)
   end
-  
+
   def evento_full_url(evento)
     evento_url(:ano => evento.data.year,:id=>evento)
   end
-  
+
   def estados_com_total_de_eventos
-    estados = Evento.agrupado_por_estado    
-    total = estados.values.inject(0){|soma,por_estado| soma += por_estado}    
+    estados = Evento.agrupado_por_estado
+    total = estados.values.inject(0){|soma,por_estado| soma += por_estado}
     return estados,total
   end
-  
+
   def meses_com_total_de_eventos
-    #gambi para heroku. Nao teve jeito 
+    #gambi para heroku. Nao teve jeito
     #heroku
     meses = Hash.new do |hash,key|
          if hash.has_key? key.to_s
@@ -28,31 +28,27 @@ module EventosHelper
          end
     end
     meses.update(Evento.agrupado_por_mes)
-    total = meses.values.inject(0){|soma,por_mes| soma += por_mes}    
+    total = meses.values.inject(0){|soma,por_mes| soma += por_mes}
     return meses,total
-  end  
+  end
 
-  def user_pic_by comentario    
+  def user_pic_by comentario
     user = User.por_login_social(comentario.twitter)
     if user
        image = user.image
-       extension = user.image[image.length-3, image.length]       
+       extension = user.image[image.length-3, image.length]
        nome_da_imagem = "#{comentario.twitter}.#{extension}"
        return link_da_imagem_do_twitter_para nome_da_imagem
     else
-      "twitter_usr_padrao.png"              
+      "twitter_usr_padrao.png"
     end
   end
-  
-  def link_da_imagem_do_twitter_para nome_da_imagem
-     EnvironmentHack.para do |env|
-       env.producao {
-          return "http://s3.amazonaws.com/twitter_images/#{nome_da_imagem}"
-       }
-       env.outros {
-          return "twimages/#{nome_da_imagem}"           
-       } 
-     end     
+
+  def link_da_imagem_do_twitter_para(nome_da_imagem)
+    if Rails.env.production?
+      "http://s3.amazonaws.com/twitter_images/#{nome_da_imagem}"
+    else
+      "twimages/#{nome_da_imagem}"
+    end
   end
-  
 end
